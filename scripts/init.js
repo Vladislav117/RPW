095bacf173c9e572428aad81f84f933d571659bb
Vladislav117 <siluetkysta@gmail.com>
2020-11-27 15:29:05 +0300

    Update init.js

--- scripts/init.js
+++ scripts/init.js
@@ -10,9 +10,9 @@ var unitType=UnitTypes.dagger;
 unitType.mineTier=1;
 unitType.mineSpeed = 6.5;
 unitType.buildSpeed=5;
-unitType.stats=new Stats();
-unitType.setStats();
 unitType.canBoost = true;
 unitType.boostMultiplier = 1.5;
+unitType.stats=new Stats();
+unitType.setStats();
 
 //UnitTypes.dagger=unitType;
