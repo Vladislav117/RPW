902e0c0b1375c498356c4334005080df33d37905
Vladislav117 <siluetkysta@gmail.com>
2020-11-27 15:32:12 +0300

    Update mech speed

--- scripts/init.js
+++ scripts/init.js
@@ -11,7 +11,7 @@ unitType.mineTier=1;
 unitType.mineSpeed = 6.5;
 unitType.buildSpeed=5;
 unitType.canBoost = true;
-unitType.boostMultiplier = 1.5;
+unitType.boostMultiplier = 2.5;
 unitType.stats=new Stats();
 unitType.setStats();
 
